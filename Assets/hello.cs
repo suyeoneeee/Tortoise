f9c4b06f3ece656c8eabbb496fff13961d0eaae5
suyeoneeee <1485ml@naver.com>
2024-11-22 11:48:00 +0900

    hello

--- Assets/hello.cs
+++ Assets/hello.cs
@@ -7,7 +7,7 @@ public class hello : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hi");
+        Debug.Log("hello");
     }
 
     // Update is called once per frame
